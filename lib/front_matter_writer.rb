b74ceb9a25b350d17ddb94bb2fe3407905ae6774
Alex Semyonov <alex@semyonov.us>
2018-11-07 01:27:11 +0300

    Introduce FrontMatterParser::Writer

--- lib/front_matter_writer.rb
+++ lib/front_matter_writer.rb
@@ -3,5 +3,6 @@
 require "front_matter_parser"
 require "front_matter_parser/dumper"
 require "front_matter_parser/syntax_writer"
+require "front_matter_parser/writer"
 
 FrontMatterWriter = FrontMatterParser
