f5cf3b5889555aef7077b64fb1cd6b0234a5b69d
Alex Semyonov <alex@semyonov.us>
2018-11-07 01:27:39 +0300

    Introduce FrontMatter namespace

---
+++ lib/front_matter.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+require "front_matter_writer"
+
+FrontMatter = FrontMatterParser
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
+require 'pry-byebug'
 require 'simplecov'
-
 SimpleCov.start
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
-require 'front_matter_parser'
-require 'pry-byebug'
+require 'front_matter'
 Dir["#{File.expand_path('support', __dir__)}/*.rb"].each do |file|
   require file
 end
